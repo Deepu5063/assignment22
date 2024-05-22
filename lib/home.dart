b3264ca726723cc413bd7e0a0acd2ce97fa21684
Deepu5063 <bhowmik15-5063@diu.edu.bd>
2024-05-22 23:57:05 +0600

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'api_photo_and_details.dart';
+import 'imgDetailsPage.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Photo Gallery App',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.w500)),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: fetchPhotos(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No photos found'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final photo = snapshot.data![index];
+                return ListTile(
+                  leading: GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>details(
+                        url: photo.thumbnailUrl,
+                        title: photo.title,
+                        Id: photo.id,
+                      ),
+                      ));
+                    },
+                    child: Image.network(photo.thumbnailUrl),
+                  ),
+                  title: Text(photo.title),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+
+
+
+
+
+  Future<List<Photo>> fetchPhotos() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((photo) => Photo.fromJson(photo)).toList();
+    } else {
+      throw Exception('Failed to load photos');
+    }
+  }
+
+
+}
+
